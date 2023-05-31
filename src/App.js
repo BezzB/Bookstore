3b5a9b6c8d41f6d525f080c9c6c859804b302fd1
BezzB <118984781+BezzB@users.noreply.github.com>
2023-05-31 18:06:41 +0300

    Upddate files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Books from './components/Booklist';
-import AddBook from './components/Bookform';
-import Catagories from './components/Categories';
-
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/categories" element={<Catagories />} />
-      </Routes>
-      <AddBook />
-    </Router>
-  );
+  return <h2>Redux Toolkit</h2>;
 }
-
 export default App;

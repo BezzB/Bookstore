4999db55ef9cca06aac18bf7c114f5e74f13e63f
BezzB <118984781+BezzB@users.noreply.github.com>
2023-06-02 10:55:22 +0300

    Create Booklist

--- src/redux/bookList.js
+++ src/redux/bookList.js
@@ -1,66 +1,45 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+import React, { useState } from 'react';
 
-import { useEffect } from 'react';
-import {
-  getBooks, removeBook, deleteBooks,
-} from './books/booksSlice';
+const Book = () => {
+  const [books, setBooks] = useState([
+    {
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ]);
 
-function BookList() {
-  const { book, isLoading } = useSelector((state) => state.book);
-  const dispatch = useDispatch();
-  const handleremoveBook = (itemId) => {
-    dispatch(removeBook(itemId));
-    dispatch(deleteBooks(itemId));
+  const deleteBook = (bookId) => {
+    setBooks(books.filter((book) => book.item_id !== bookId));
   };
 
-  useEffect(() => {
-    dispatch(getBooks());
-  }, [dispatch]);
-
-  if (isLoading) {
-    return (
-      <div className="loading">
-        <p>Loading...</p>
-      </div>
-    );
-  }
   return (
-    <div className="imp-book">
-      {book.map((key) => (
-        <div key={key.item_id} className="book-cont">
-          <div className="author-section">
-            <div className="book-author">
-              <p className="cat">{key.category}</p>
-              <h4 className="aut">{key.title}</h4>
-              <p className="tit">{key.author}</p>
-            </div>
-            <div className="book-btn">
-              <button type="button" className="l-btn">Comments</button>
-              <button className="l-btn rem" type="button" onClick={() => handleremoveBook(key.item_id)}>Delete</button>
-              <button type="button" className="l-btn">Edit</button>
-            </div>
-          </div>
-          <div className="comp-icon">
-            <div className="progress">
-              <AiOutlineLoading3Quarters />
-            </div>
-            <div className="prog-details">
-              <span className="comp-percent">54%</span>
-              <span className="comp-status">completed</span>
-            </div>
-          </div>
-          <div className="current-cont">
-            <p className="current-chpt">CURRENT CHAPTER</p>
-            <p className="chapter-1">
-              Chapter 15
-            </p>
-            <button type="button" className="upBtn">UPDATE LEVEL</button>
-          </div>
-        </div>
-      ))}
-
+    <div>
+      <ul>
+        {books.map((book) => (
+          <li key={book.item_id}>
+            <h2>{book.title}</h2>
+            <p>{book.author}</p>
+            <p>{book.category}</p>
+            <button type="button" onClick={() => deleteBook(book.item_id)}>Delete Book</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
-export default BookList;
+};
+
+export default Book;

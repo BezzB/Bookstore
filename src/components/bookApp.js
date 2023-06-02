86e89b59fb4181fd24a88fb26a93d6e2ae500dd0
BezzB <118984781+BezzB@users.noreply.github.com>
2023-06-02 12:49:26 +0300

    Update list

--- src/components/bookApp.js
+++ src/components/bookApp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Forms from '../redux/bookForm';
-import BookList from '../redux/bookLists';
+import BookList from '../redux/Lists';
 
 function Books() {
   return (
